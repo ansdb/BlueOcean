4fd7eecf2f0fdffc8ef801911aa4efb29d243788
ansdb <anisdabdi0x@gmail.com>
2020-08-21 12:31:18 +0100

    Update user data

--- roles/create_instance/files/user_data.sh
+++ roles/create_instance/files/user_data.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e -x
 export DEBIAN_FRONTEND=noninteractive
+sudp apt-get update -y
 sudo locale-gen pt_BR.UTF-8
 sudo apt-get install -y nginx
 cat > /etc/nginx/sites-enabled/default << EOF
